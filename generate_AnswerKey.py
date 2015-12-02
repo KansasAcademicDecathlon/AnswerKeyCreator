b9d5c5fe0f33956e3c2df055412f2f52a3a1e174
Paul Vittorino <paul@vittorino.net>
2015-12-01 20:30:06 -0600

    Add function to create tests
    Added a create_test function to generate the test dictionaries
    from arguments.

--- generate_AnswerKey.py
+++ generate_AnswerKey.py
@@ -1,4 +1,11 @@
 
+def create_test(number, name, answers):
+    test = {'TotalQuestions':question_count, 'PointsPer':points_per_question, 'TotalPoints':total_points};
+    test['EventNum'] = number
+    test['EventName'] = name
+    test['answers'] = answers
+    return test
+
 def write_answers(test):
     title = test['EventNum'] + ' - ' + test['EventName']
     output = ','.join([title,test['EventNum'],test['TotalQuestions'],test['PointsPer'],test['TotalPoints']])
@@ -14,9 +21,9 @@ lang_answers = ('A','D','B','D','C','C','E','E','E','A','B','D','C','E','A','D',
 music_answers = ('D','B','B','D','C','C','A','B','B','A','B','B','B','C','E','D','A','C','A','E','E','E','B','E','E','D','A','E','E','D','D','A','C','E','A','C','B','A','E','B','C','A','E','D','B','C','D','C','D','D')
 science_answers = ('B','D','A','E','C','D','A','B','D','C','C','D','A','E','B','A','B','C','D','E','A','E','B','A','D','E','C','A','C','D','A','E','B','C','E','B','D','E','C','E','B','B','B','A','C','B','A','D','B','E')
 
-lang_test = {'EventNum':'1', 'EventName':'Lang & Lit', 'TotalQuestions':question_count, 'PointsPer':points_per_question, 'TotalPoints':total_points, 'answers':lang_answers}
-music_test = {'EventNum':'2', 'EventName':'Music',  'TotalQuestions':question_count, 'PointsPer':points_per_question, 'TotalPoints':total_points, 'answers':music_answers}
-science_test = {'EventNum':'11', 'EventName':'Science', 'TotalQuestions':question_count, 'PointsPer':points_per_question, 'TotalPoints':total_points, 'answers':science_answers}
+lang_test = create_test('1', 'Lang & Lit',lang_answers)
+music_test = create_test('2', 'Music', music_answers)
+science_test = create_test('11', 'Science', science_answers)
 
 test_list = (lang_test,music_test,science_test)
 
